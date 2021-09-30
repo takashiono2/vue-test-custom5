1c0235ee0846d31c41fbae7a5845e0dc7f08bc71
takashiono2 <takashiono710@gmail.com>
2021-09-30 18:39:53 +0900

    フォーム編集前

--- nuxt/pages/users/_id.vue
+++ nuxt/pages/users/_id.vue
@@ -3,7 +3,21 @@
     パラメータ：{{ $route.params.id }}
     <!-- todos一覧：{{ todoStates　}} -->
     <ul v-for="todo in todoStates" :key="todo.id">
-      <li>名前:{{ todo.name }}</li>
+    <form @submit.prevent ="editAdd">
+      <input type="text"
+        v-model="name"
+        class="text-input"
+        placeholder="タスクを入力してください"
+        @focusin="startEditing"
+        @focusout="finishEditing"
+      >
+      <v-btn>Add</v-btn>
+    </form>
+      <li>
+        <p>名前:{{ todo.name }}</p>
+        <p>ID:{{todo.id}}</p>
+        <p>状態：{{todoStates}}</p>
+      </li>
     </ul>
   </div>
 </template>
@@ -13,17 +27,33 @@
     layout: 'default',
     data(){
       return {
-        todos:[]
+        todos:[],
+        name:''
+      }
+    },
+    methods:{
+      startEditing() {
+        this.isEditing = true
+      },
+      finishEditing() {
+        this.isEditing = false
       }
     },
     created: function() {
       this.$store.dispatch('todos/init')
     },
     computed:{
+      id(){
+        return this.$route.params.id
+      },
       todoStates(){
-        return this.$store.getters['todos/orderdTodos']
-        // return this.$store.state.todos.todos
+        this.todos = this.$store.getters['todos/orderdTodos']
+        return this.todos.filter((todo)=>{
+          return todo.id === this.id
+        })
       }
+        // return this.todos.find((todo)=> todo.id === "this.id")
+        // return this.$store.state.todos.todos
     },
     // props: {
     //   name: 'users-id',
